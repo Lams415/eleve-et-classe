dea36170f2ac783fd519a4bf69201689649efdfa
Lams415 <amsellemeyal93100@gmail.com>
2025-02-15 18:23:10 +0200

    je suis entrain de cree des éleve

---
+++ script.js
@@ -0,0 +1,40 @@
+const Note = document.querySelector("body p:first-of-type");
+const  NotePREMIER = document.querySelector("body p:nth-of-type(2)");
+const  NoteDEUSIEME= document.querySelector("body p:nth-of-type(3)");
+const NoteTroisième = document.querySelector("body p:last-of-type");
+
+var EleveScientique = [{
+
+    "nomEtPrenom": " eyal amsellem",
+
+    "Math": 10,
+    "Anglais": 10,
+    "Science": 18,
+
+},{
+
+    "nomEtPrenom": " libi amsellem",
+
+    "Math": 10,
+    "Anglais": 20,
+    "Science": 8,
+
+},{
+    "nomEtPrenom": " lihi amsellem",
+
+    "Math": 10,
+    "Anglais": 20,
+    "Science": 18,
+
+},{
+    "nomEtPrenom": " shani amsellem",
+
+    "Math": 10,
+    "Anglais": 0,
+    "Science": 8,
+
+
+
+}];
+
+
